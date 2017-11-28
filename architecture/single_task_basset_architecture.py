be44fe72d1a81bbf6f937b0b8ab9257bf44fb6ca
trimcrae <trimcrae@gmail.com>
2017-11-28 14:28:17 -0800

    Fixed tensor shapes

--- architecture/single_task_basset_architecture.py
+++ architecture/single_task_basset_architecture.py
@@ -17,38 +17,48 @@ import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 import pandas
 import time
+import pydot
+import graphviz
+from keras.utils import plot_model
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+#import tensorflow as tf
+
+
+
 
 start_time = time.time()
 
 np.random.seed(1234) #sets random seed for consistency
 seq_length = 2000 #2kb iput
 num_labels = 1    #Our binary final prediction. The only label is the predicted probability that the gene is on or whatever
-num_epochs = 2	  #Number of epochs to train for
+num_epochs = 25	  #Number of epochs to train for
 save_folder = "Weights/"	#Folder where weights/parameters are stored
 save_path = save_folder+"model_weights.h5" #Location to save weights/parameters to
 ##########################
-init_weights_file = None#'Weights/model_weights.h5' #load in pre-trained weights/parameters Anna gave us here if we're using them. Elsewise, leave as None.
+init_weights_file = None#'Weights/model_weights_MSE.h5' #load in pre-trained weights/parameters Anna gave us here if we're using them. Elsewise, leave as None.
 ##########################
  
 #Train Chromosomes 1 2 3 4 7 8 9 10 11 12 13 14 15 17 19 20 21 22
 #Val Chromosomes 16 18
 #Test Chromosomes  5 6
-train = pandas.read_csv("train.csv", sep = ',').values 
-val = pandas.read_csv("val.csv", sep = ',').values
-test = pandas.read_csv("test.csv", sep = ',').values 
+
+train = pandas.read_csv("hsa_train.csv", sep = ',').values 
+val = pandas.read_csv("hsa_val.csv", sep = ',').values
+#test = pandas.read_csv("test.csv", sep = ',').values 
 
 train = np.delete(train, 0, axis = 1)
 val = np.delete(val, 0, axis =1)
-test = np.delete(test, 0, axis =1)
+#test = np.delete(test, 0, axis =1)
 
 np.random.shuffle(train)
 np.random.shuffle(val)
-np.random.shuffle(test)
+#np.random.shuffle(test)
 
 print("Data read in after " + str(time.time()-start_time))
 
 
-num_val_examples = len(val)
+num_val_examples = min(20000,len(val))
 val_nums = np.empty([num_val_examples, 2001])
 for i in range(num_val_examples):#len(train)):
 	seq_list = list(val[i,0])
@@ -68,7 +78,7 @@ Y_val = val_nums[:, 2000]
 print("Val data converted after " + str(time.time()-start_time))
 
 
-num_training_examples = 20000#len(train)/10
+num_training_examples = min(20000,len(train))
 
 print(str(num_training_examples) + " training examples")
 
@@ -91,35 +101,35 @@ Y_train = train_nums[:, 2000]
 print("Train data converted after " + str(time.time()-start_time))
 
 
-
-#Generate test data. This is just to test the architecture. X and Y will be loaded in from real data when this is actually used.
-#X_train_seq = np.floor((np.random.rand(100,seq_length)*4)).astype(int)
-#X_test_seq = np.floor((np.random.rand(100,seq_length)*4)).astype(int)
-
 X_train = []
 X_val = []
-#Y_train = []
-#Y_test = []
 
-#Y_train = np.floor((np.random.rand(100)*2)).astype(int)
 for i in range(num_training_examples):
 	X_train.append(to_categorical(X_train_seq[i, :], num_classes=4))
-X_train = np.moveaxis(X_train, 2, 0)
-X_train = X_train.reshape(num_training_examples, 4, seq_length, 1)
+
+print(np.shape(X_train))
+X_train = np.asarray(X_train)
+#X_train = np.moveaxis(X_train, 2, 0) #These reshapes might be causing problemsX_train = tf.convert_to_tensor(X_train)
+#print(np.shape(X_train))
 
 
-#Y_val = np.floor((np.random.rand(100)*2)).astype(int)
+X_train = np.expand_dims(X_train, axis = 1) #These reshapes might be causing problems should be (num_examples, 1, sequence_length, 4)
+
+print(np.shape(X_train))
+
+
 for i in range(num_val_examples):
 	X_val.append(to_categorical(X_val_seq[i, :], num_classes=4))
-X_val = np.moveaxis(X_val, 2, 0)
-X_val = X_val.reshape(num_val_examples, 4, seq_length, 1)
+X_val = np.asarray(X_val)
+X_val = np.expand_dims(X_val, axis = 1) #These reshapes might be causing problems should be (num_examples, 1, sequence_length, 4)
 
 print("Converted to one-hot after " + str(time.time()-start_time))
 
 
 #Build model based heavily on original Basset architecture
 model = Sequential() #Multiple models can be made, each corresponding to a different species
-model.add(Convolution2D(300,(4,19),input_shape=(4,int(seq_length), 1)))	
+model.add(Convolution2D(300,(1,19),input_shape=(1,int(seq_length), 4))) #the 1 and the 4 should be switched here. Correct order would be 300,(1,19) and (1,int(seq_length), 4)))
+model.add(BatchNormalization(axis=1))	#Isn't in all versions but is good
 model.add(PReLU())
 model.add(MaxPooling2D(pool_size=(1,3)))
 
@@ -146,7 +156,12 @@ model.add(Dense(num_labels))
 model.add(Activation("sigmoid"))
 
 adam = keras.optimizers.Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-model.compile(loss="binary_crossentropy",optimizer=adam) 
+model.compile(loss="mean_squared_error",optimizer=adam,
+              metrics=['accuracy']) 
+
+#plot_model(model, to_file='model.png')
+#SVG(model_to_dot(model).create(prog='dot', format='svg'))
+#print(model.summary())
 
 #Load weights if they exist
 if init_weights_file!=None:
@@ -162,7 +177,20 @@ print("Trained after " + str(time.time()-start_time))
 y_probs = model.predict_proba(X_val)							#Predicted probability that each label is positive
 AU_PRC = average_precision_score(Y_val, y_probs)				#Area under PR curve taken as average of precisions for all recalls
 precision, recall, _ = precision_recall_curve(Y_val, y_probs)	#Arrays for plotting precision recall curve
-print(AU_PRC)
+print('Max prob:', np.amax(y_probs))
+print('Min prob:', np.amin(y_probs))
+print('AUPRC:', AU_PRC)
+#score = model.evaluate(X_train, Y_train, verbose=0)
+#print('Train loss:', score[0])
+#print('Train accuracy:', score[1])
+score = model.evaluate(X_val, Y_val, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#print(precision)
+#print(recall)
+
+
 
 
 
@@ -178,6 +206,7 @@ plt.title('2-class Precision-Recall curve: AUC={0:0.2f}'.format(AU_PRC))
 plt.savefig("AUPRC.png")
 '''
 #Save Parameters
+
 if init_weights_file!=None:
     model.save_weights(init_weights_file)	
 else:
@@ -190,9 +219,11 @@ else:
 	model.save_weights(save_path)
 
 
-#Display plots if they exist
+
 print("Finished after " + str(time.time()-start_time))
 
+
+#Display plots if they exist
 #plt.show()
 
 
