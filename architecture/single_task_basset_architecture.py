65e58804a7071f37aae7958395c2c0bcd99e3c03
trimcrae <trimcrae@gmail.com>
2017-11-29 14:47:40 -0800

    Really fixed the tensor dimensions this time

--- architecture/single_task_basset_architecture.py
+++ architecture/single_task_basset_architecture.py
@@ -32,11 +32,11 @@ start_time = time.time()
 np.random.seed(1234) #sets random seed for consistency
 seq_length = 2000 #2kb iput
 num_labels = 1    #Our binary final prediction. The only label is the predicted probability that the gene is on or whatever
-num_epochs = 25	  #Number of epochs to train for
+num_epochs = 10	  #Number of epochs to train for
 save_folder = "Weights/"	#Folder where weights/parameters are stored
 save_path = save_folder+"model_weights.h5" #Location to save weights/parameters to
 ##########################
-init_weights_file = None#'Weights/model_weights_MSE.h5' #load in pre-trained weights/parameters Anna gave us here if we're using them. Elsewise, leave as None.
+init_weights_file = None#'Weights/model_weights.h5' #load in pre-trained weights/parameters Anna gave us here if we're using them. Elsewise, leave as None.
 ##########################
  
 #Train Chromosomes 1 2 3 4 7 8 9 10 11 12 13 14 15 17 19 20 21 22
@@ -44,6 +44,7 @@ init_weights_file = None#'Weights/model_weights_MSE.h5' #load in pre-trained wei
 #Test Chromosomes  5 6
 
 train = pandas.read_csv("hsa_train.csv", sep = ',').values 
+print("Training data read in after"+ str(time.time()-start_time))
 val = pandas.read_csv("hsa_val.csv", sep = ',').values
 #test = pandas.read_csv("test.csv", sep = ',').values 
 
@@ -55,7 +56,7 @@ np.random.shuffle(train)
 np.random.shuffle(val)
 #np.random.shuffle(test)
 
-print("Data read in after " + str(time.time()-start_time))
+print("All data read in after " + str(time.time()-start_time))
 
 
 num_val_examples = min(20000,len(val))
@@ -107,7 +108,7 @@ X_val = []
 for i in range(num_training_examples):
 	X_train.append(to_categorical(X_train_seq[i, :], num_classes=4))
 
-print(np.shape(X_train))
+#print(np.shape(X_train))
 X_train = np.asarray(X_train)
 #X_train = np.moveaxis(X_train, 2, 0) #These reshapes might be causing problemsX_train = tf.convert_to_tensor(X_train)
 #print(np.shape(X_train))
@@ -129,27 +130,29 @@ print("Converted to one-hot after " + str(time.time()-start_time))
 #Build model based heavily on original Basset architecture
 model = Sequential() #Multiple models can be made, each corresponding to a different species
 model.add(Convolution2D(300,(1,19),input_shape=(1,int(seq_length), 4))) #the 1 and the 4 should be switched here. Correct order would be 300,(1,19) and (1,int(seq_length), 4)))
-model.add(BatchNormalization(axis=1))	#Isn't in all versions but is good
-model.add(PReLU())
+model.add(BatchNormalization(axis=-1))	#Isn't in all versions but is good
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(1,3)))
 
-model.add(Convolution2D(200,(1,11), kernel_constraint=maxnorm(7)))
-model.add(BatchNormalization(axis=1))
-model.add(PReLU())
+model.add(Convolution2D(filters=200,kernel_size=(1,11)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(1,4)))
 
-model.add(Convolution2D(200,(1,7),kernel_constraint=maxnorm(7)))
-model.add(BatchNormalization(axis=1))
-model.add(PReLU())
+model.add(Convolution2D(filters=200,kernel_size=(1,7)))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(1,4)))
 
 model.add(Flatten())
-model.add(Dense(1000,activity_regularizer=l1(0.00001),kernel_constraint=maxnorm(7)))
-model.add(PReLU())
+model.add(Dense(1000))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
 model.add(Dropout(0.3))
 
-model.add(Dense(1000,activity_regularizer=l1(0.00001),kernel_constraint=maxnorm(7)))
-model.add(PReLU())
+model.add(Dense(1000))
+model.add(BatchNormalization(axis=-1))
+model.add(Activation('relu'))
 model.add(Dropout(0.3))
 
 model.add(Dense(num_labels)) 
@@ -174,18 +177,26 @@ model.fit(X_train, Y_train, batch_size=32, epochs=num_epochs, verbose=1)
 print("Trained after " + str(time.time()-start_time))
 
 #Evaluate Model
+y_probs = model.predict_proba(X_train)							#Predicted probability that each label is positive
+AU_PRC = average_precision_score(Y_train, y_probs)				#Area under PR curve taken as average of precisions for all recalls
+precision, recall, _ = precision_recall_curve(Y_train, y_probs)	#Arrays for plotting precision recall curve
+print('Max prob Train:', np.amax(y_probs))
+print('Min prob Train:', np.amin(y_probs))
+print('Train AUPRC:', AU_PRC)
+
+
 y_probs = model.predict_proba(X_val)							#Predicted probability that each label is positive
 AU_PRC = average_precision_score(Y_val, y_probs)				#Area under PR curve taken as average of precisions for all recalls
 precision, recall, _ = precision_recall_curve(Y_val, y_probs)	#Arrays for plotting precision recall curve
-print('Max prob:', np.amax(y_probs))
-print('Min prob:', np.amin(y_probs))
-print('AUPRC:', AU_PRC)
+print('Max prob val:', np.amax(y_probs))
+print('Min prob val:', np.amin(y_probs))
+print('Val AUPRC:', AU_PRC)
 #score = model.evaluate(X_train, Y_train, verbose=0)
 #print('Train loss:', score[0])
 #print('Train accuracy:', score[1])
 score = model.evaluate(X_val, Y_val, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+print('Val loss:', score[0])
+print('Val accuracy:', score[1])
 
 #print(precision)
 #print(recall)
