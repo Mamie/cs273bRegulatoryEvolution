a4a03150a5eb0b2afd77196a8c28cf4f9d711e6e
Mamie wang <szmamie@stanford.edu>
2017-12-07 22:16:12 +0000

    Add argument parsing for one hot encoding pipeline

--- preprocess/digitizeSequences.py
+++ preprocess/digitizeSequences.py
@@ -4,8 +4,11 @@
     File name: digitizeSequences.py
     Author: Mamie Wang
     Date created: 11/29/2017
-    Date last modified: 11/29/2017
+    Date last modified: 12/06/2017
     Python version: 3.6
+
+    Usage:
+	python digitizeSequences.py "path to training csv" "path to validation csv"
 
     Input: csv file of training and validation data
     Output: numpy memmap file for digitized (acgt -> 0123) training and validation sequences
@@ -36,78 +39,80 @@ from keras.utils import plot_model
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
 #import tensorflow as tf
+import argparse
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('trainingcsvPath')
+	parser.add_argument('validationcsvPath')
+	arguments = parser.parse_args()
+	trainPath = arguments.trainingcsvPath
+	valPath = arguments.validationcsvPath
+	start_time = time.time()
+	print('Start reading in files...')
+
+	np.random.seed(1234) #sets random seed for consistency
+	seq_length = 2000 #2kb iput
+	num_labels = 1    #Our binary final prediction. The only label is the predicted probability that the gene is on or whatever
+	init_weights_file = None#'Weights/model_weights_MSE.h5' #load in pre-trained weights/parameters Anna gave us here if we're using them. Elsewise, leave as None.
 
 
+	train = pandas.read_csv(trainPath, sep = ',').values 
+	val = pandas.read_csv(valPath, sep = ',').values
 
-start_time = time.time()
+	train = np.delete(train, 0, axis = 1)
+	val = np.delete(val, 0, axis =1)
 
-np.random.seed(1234) #sets random seed for consistency
-seq_length = 2000 #2kb iput
-num_labels = 1    #Our binary final prediction. The only label is the predicted probability that the gene is on or whatever
-init_weights_file = None#'Weights/model_weights_MSE.h5' #load in pre-trained weights/parameters Anna gave us here if we're using them. Elsewise, leave as None.
-##########################
- 
-#Train Chromosomes 1 2 3 4 7 8 9 10 11 12 13 14 15 17 19 20 21 22
-#Val Chromosomes 16 18
-#Test Chromosomes  5 6
+	np.random.shuffle(train)
+	np.random.shuffle(val)
 
-train = pandas.read_csv("/data/processed/hsa/hsa_train.csv", sep = ',').values 
-val = pandas.read_csv("/data/processed/hsa/hsa_val.csv", sep = ',').values
-#test = pandas.read_csv("test.csv", sep = ',').values 
+	print("Data read in after " + str(time.time()-start_time))
 
-train = np.delete(train, 0, axis = 1)
-val = np.delete(val, 0, axis =1)
-#test = np.delete(test, 0, axis =1)
+	num_val_examples = len(val)
+	print(str(num_val_examples) + " validation examples")
+	filename = os.path.split(valPath)[0] # '/data/processed/hsa/validationDigitized.dat' 
+	filename = filename + '/validationDigitized.dat'
+	val_nums = np.memmap(filename, dtype=np.int32, mode='w+', shape=(num_val_examples, 2001))
 
-np.random.shuffle(train)
-np.random.shuffle(val)
-#np.random.shuffle(test)
-
-print("Data read in after " + str(time.time()-start_time))
-
-num_val_examples = len(val)
-print(str(num_val_examples) + " validation examples")
-filename = '/data/processed/hsa/validationDigitized.dat' 
-val_nums = np.memmap(filename, dtype=np.int32, mode='w+', shape=(num_val_examples, 2001))
-
-start_time = time.time()
-print("convert acgt to 0123 for validation data....")
-for i in range(num_val_examples):#len(train)):
-	seq_list = list(val[i,0])
-	for j in range(seq_length):
-		if seq_list[j].lower() == 'a':
-			val_nums[i, j] = 0
-		elif seq_list[j].lower() == 'c':
-			val_nums[i, j] = 1
-		elif seq_list[j].lower() == 'g':
-			val_nums[i, j] = 2
-		elif seq_list[j].lower() == 't':
-			val_nums[i, j] = 3
-	val_nums[i, 2000] = val[i,1]
-print("Val data converted to digits after " + str(time.time()-start_time))
-print(val_nums.shape)
-del val_nums
+	start_time = time.time()
+	print("convert acgt to 0123 for validation data....")
+	for i in range(num_val_examples):#len(train)):
+		seq_list = list(val[i,0])
+		for j in range(seq_length):
+			if seq_list[j].lower() == 'a':
+				val_nums[i, j] = 0
+			elif seq_list[j].lower() == 'c':
+				val_nums[i, j] = 1
+			elif seq_list[j].lower() == 'g':
+				val_nums[i, j] = 2
+			elif seq_list[j].lower() == 't':
+				val_nums[i, j] = 3
+		val_nums[i, 2000] = val[i,1]
+	print("Val data converted to digits after " + str(time.time()-start_time))
+	print(val_nums.shape)
+	del val_nums
 
 
-num_training_examples = len(train)
-print(str(num_training_examples) + " training examples")
-filename = '/data/processed/hsa/trainDigitized.dat'
-train_nums = np.memmap(filename, dtype=np.int32, mode='w+', shape=(num_training_examples, 2001))
-start_time = time.time()
-print("convert acgt to 0123 for training data...")
-for i in range(num_training_examples):
-	seq_list = list(train[i,0])
-	for j in range(seq_length):
-		if seq_list[j].lower() == 'a':
-			train_nums[i, j] = 0
-		elif seq_list[j].lower() == 'c':
-			train_nums[i, j] = 1
-		elif seq_list[j].lower() == 'g':
-			train_nums[i, j] = 2
-		elif seq_list[j].lower() == 't':
-			train_nums[i, j] = 3
-	train_nums[i, 2000] = train[i,1]
-print("Train data converted after " + str(time.time()-start_time))
-print(train_nums.shape)
-del train_nums
+	num_training_examples = len(train)
+	print(str(num_training_examples) + " training examples")
+	filename = os.path.split(trainPath)[0]
+	filename = filename + '/trainDigitized.dat' # '/data/processed/hsa/trainDigitized.dat'
+	train_nums = np.memmap(filename, dtype=np.int32, mode='w+', shape=(num_training_examples, 2001))
+	start_time = time.time()
+	print("convert acgt to 0123 for training data...")
+	for i in range(num_training_examples):
+		seq_list = list(train[i,0])
+		for j in range(seq_length):
+			if seq_list[j].lower() == 'a':
+				train_nums[i, j] = 0
+			elif seq_list[j].lower() == 'c':
+				train_nums[i, j] = 1
+			elif seq_list[j].lower() == 'g':
+				train_nums[i, j] = 2
+			elif seq_list[j].lower() == 't':
+				train_nums[i, j] = 3
+		train_nums[i, 2000] = train[i,1]
+	print("Train data converted after " + str(time.time()-start_time))
+	print(train_nums.shape)
+	del train_nums
 
--- preprocess/oneHotEncoding.py
+++ preprocess/oneHotEncoding.py
@@ -4,9 +4,11 @@
     File name: oneHotEncoding.py
     Author: Mamie Wang
     Date created: 11/29/2017
-    Date last modified: 11/29/2017
+    Date last modified: 12/06/2017
     Python version: 3.6
 
+    Usage:
+	python oneHotEncoding.py "path to the training data" "path to the validation data"
     Input: numpy memmap of the validation and training data (digitized)
     Output: one hot encoded array of validation and training data
 """
@@ -35,56 +37,65 @@ from keras.utils import plot_model
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
 #import tensorflow as tf
+import argparse
 
+if __name__=='__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('trainDat')
+	parser.add_argument('valDat')
+	arguments = parser.parse_args()
+	trainPath = arguments.trainDat
+	valPath = arguments.valDat
 
+	start_time = time.time()
+	print('Start reading in files...')
 
+	np.random.seed(1234) #sets random seed for consistency
+	seq_length = 2000 #2kb iput
+	num_labels = 1    #Our binary final prediction. The only label is the predicted probability that the gene is on or whatever
 
-start_time = time.time()
+	num_val_examples = 315426
+	num_train_examples = 4246422
 
-np.random.seed(1234) #sets random seed for consistency
-seq_length = 2000 #2kb iput
-num_labels = 1    #Our binary final prediction. The only label is the predicted probability that the gene is on or whatever
+	print('Reading in validation data')
+	val_nums = np.memmap(valPath, dtype=np.int32, mode='r', shape=(num_val_examples, 2001))
+	print('Read in ' + str(time.time() - start_time))
+	basename = os.path.split(valPath)[0]
+	datafile = basename + '/validationOneHotEncoding.dat'
+	datafile2 = basename + '/validationLabel.dat'
+	X_val = np.memmap(datafile, dtype=np.uint8, mode='w+', shape=(num_val_examples, 1, 2000, 4))
+	Y_val = np.memmap(datafile2, dtype=np.uint8, mode='w+', shape=(num_val_examples))
+	print('Start one hot encoding on validation data')
+	start_time = time.time()
+	for i in range(num_val_examples):
+		X_val[i, 0, :, :] = to_categorical(val_nums[i, 0:2000], num_classes=4)
+		Y_val[i] = val_nums[i, 2000]
+	print('Complete one hot encoding in ' + str(time.time() - start_time))
+	print(X_val.shape)
+	print(Y_val.shape)
+	del X_val
+	del Y_val
+	del val_nums
 
-num_val_examples = 315426
-num_train_examples = 4246422
-
-print('Reading in validation data')
-val_nums = np.memmap('/data/processed/hsa/validationDigitized.dat', dtype=np.int32, mode='r', shape=(num_val_examples, 2001))
-print('Read in ' + str(time.time() - start_time))
-datafile = '/data/processed/hsa/validationOneHotEncoding.dat'
-datafile2 = '/data/processed/hsa/validationLabel.dat'
-X_val = np.memmap(datafile, dtype=np.uint8, mode='w+', shape=(num_val_examples, 1, 2000, 4))
-Y_val = np.memmap(datafile2, dtype=np.uint8, mode='w+', shape=(num_val_examples))
-print('Start one hot encoding on validation data')
-start_time = time.time()
-for i in range(num_val_examples):
-	X_val[i, 0, :, :] = to_categorical(val_nums[i, 0:2000], num_classes=4)
-	Y_val[i] = val_nums[i, 2000]
-print('Complete one hot encoding in ' + str(time.time() - start_time))
-print(X_val.shape)
-print(Y_val.shape)
-del X_val
-del Y_val
-del val_nums
-
-start_time = time.time()
-print('Reading in training data')
-train_nums = np.memmap('/data/processed/hsa/trainDigitized.dat', dtype=np.int32, mode='r', shape=(num_train_examples, 2001))
-print('Read in ' + str(time.time() - start_time))
-datafile = '/data/processed/hsa/trainOneHotEncoding.dat'
-datafile2 = '/data/processed/hsa/trainOneHotLabel.dat'
-X_train = np.memmap(datafile, dtype=np.uint8, mode='w+', shape=(num_train_examples, 1, 2000, 4))
-Y_train = np.memmap(datafile2, dtype=np.uint8, mode='w+', shape=(num_train_examples))
-print('Start one hot encoding on training data')
-start_time = time.time()
-for i in range(num_train_examples):
-	X_train[i, 0, :, :] = to_categorical(train_nums[i, 0:2000], num_classes=4)
-	Y_train[i] = train_nums[i, 2000]
-print('Complete one hot encoding in ' + str(time.time() - start_time))
-print(X_train.shape)
-print(Y_train.shape)
-del X_train
-del Y_train
-del train_nums
+	start_time = time.time()
+	print('Reading in training data')
+	train_nums = np.memmap(trainPath, dtype=np.int32, mode='r', shape=(num_train_examples, 2001))
+	print('Read in ' + str(time.time() - start_time))
+	basename = os.path.split(trainPath)[0]
+	datafile = basename + '/trainOneHotEncoding.dat'
+	datafile2 = basename + '/trainOneHotLabel.dat'
+	X_train = np.memmap(datafile, dtype=np.uint8, mode='w+', shape=(num_train_examples, 1, 2000, 4))
+	Y_train = np.memmap(datafile2, dtype=np.uint8, mode='w+', shape=(num_train_examples))
+	print('Start one hot encoding on training data')
+	start_time = time.time()
+	for i in range(num_train_examples):
+		X_train[i, 0, :, :] = to_categorical(train_nums[i, 0:2000], num_classes=4)
+		Y_train[i] = train_nums[i, 2000]
+	print('Complete one hot encoding in ' + str(time.time() - start_time))
+	print(X_train.shape)
+	print(Y_train.shape)
+	del X_train
+	del Y_train
+	del train_nums
 
 
